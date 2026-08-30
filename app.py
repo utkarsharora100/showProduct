1e9683b9f5ecebbb3a073971ebeaeeaae8aa1e88
utkarsharora100 <utkarsharora100@gmail.com>
2026-08-30 20:19:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+products = [
+    {
+        "id": 1,
+        "name": "Cloud 1",
+        "price": 99.99
+    },
+    {
+        "id": 2,
+        "name": "Cloud 2",
+        "price": 149.99
+    },
+    {
+        "id": 3,
+        "name": "Cloud 3",
+        "price": 199.99
+    },
+    
+]
+
+
+@app.route("/showProduct", methods=["GET"])
+def show_product():
+    return jsonify(products)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
