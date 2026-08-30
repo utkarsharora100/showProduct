4a219e779f35d736e7ce21c478c39bf53a59dbe9
utkarsharora100 <utkarsharora100@gmail.com>
2026-08-30 20:34:54 +0530

    added health

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ products = [
 def show_product():
     return jsonify(products)
 
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
